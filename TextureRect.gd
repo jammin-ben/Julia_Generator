03549899b31f920285d8675310a224721ba88d27
jammin-ben <benjamin.reber19@houghton.edu>
2020-07-24 11:38:09 -0400

    added continuous iter change

--- TextureRect.gd
+++ TextureRect.gd
@@ -1,13 +1,21 @@
 extends TextureRect
 const SPEED = .1
+const MAX_ITER_ACCEL = 50
+
+onready var num_iter = float(self.material.get_shader_param("num_iter"))
+
+var iter_speed = 12 #iter changes per second
+var iter_accel = 0
+
 func _process(delta):
-	if(Input.is_action_just_pressed("iterup")):
-		var num_iter = self.material.get_shader_param("num_iter")
-		self.material.set_shader_param("num_iter",num_iter + 1)
-	if(Input.is_action_just_pressed("iterdown")):
-		var num_iter = self.material.get_shader_param("num_iter")
-		self.material.set_shader_param("num_iter",num_iter - 1)
-		
+	var iter_delta = Input.get_action_strength("iterup") - Input.get_action_strength("iterdown")
+	if iter_delta != 0:
+		num_iter += (iter_speed + iter_accel) * delta * iter_delta
+		self.material.set_shader_param("num_iter",num_iter)
+		iter_accel += 3*delta
+		iter_accel = min(iter_accel,MAX_ITER_ACCEL)
+	else:
+		iter_accel = 0
 	var lr = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var ud = Input.get_action_strength("up") - Input.get_action_strength("down")
 	if lr !=0 or ud != 0:
