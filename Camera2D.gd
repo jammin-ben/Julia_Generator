cc89ad0c92a7286eae359f7c628fd32dcc52f3a0
jammin-ben <benjamin.reber19@houghton.edu>
2020-07-24 14:17:29 -0400

    slowed c change as function of zoom level

--- Camera2D.gd
+++ Camera2D.gd
@@ -3,7 +3,7 @@ extends Camera2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+signal zoom_changed(zoom)
 const SPEED = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,8 +13,11 @@ func _ready():
 func _process(delta):
 	if(Input.is_action_pressed("ui_accept")):
 		self.zoom *= .97 
+		emit_signal("zoom_changed",self.zoom)
 	if Input.is_action_pressed("ui_cancel"):	
 		self.zoom *= 1.025
+		emit_signal("zoom_changed",self.zoom)
+		
 	if(Input.is_action_pressed("ui_left")):
 		self.position.x -= SPEED * self.zoom.x * delta 
 	
--- TextureRect.gd
+++ TextureRect.gd
@@ -6,7 +6,7 @@ onready var num_iter = float(self.material.get_shader_param("num_iter"))
 
 var iter_speed = 12 #iter changes per second
 var iter_accel = 0
-
+var camera_zoom = 1
 signal c_changed(c)
 
 func _process(delta):
@@ -19,12 +19,14 @@ func _process(delta):
 		iter_accel = min(iter_accel,MAX_ITER_ACCEL)
 	else:
 		iter_accel = 0
+		
+	#move c value
 	var lr = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var ud = Input.get_action_strength("up") - Input.get_action_strength("down")
 	if lr !=0 or ud != 0:
 		var c = self.material.get_shader_param("c")
-		c.x += lr * SPEED * delta
-		c.y += ud * SPEED * delta
+		c.x += lr * SPEED * delta * (1 - (1 - camera_zoom)*(1-camera_zoom))
+		c.y += ud * SPEED * delta * (1 - (1 - camera_zoom)*(1-camera_zoom))
 		self.material.set_shader_param("c",c)
 		emit_signal("c_changed",c)
 	
@@ -35,3 +37,6 @@ func _on_ColorPicker_color_changed(color):
 	
 	self.material.set_shader_param("g",color.g)
 	self.material.set_shader_param("b",color.b)
+
+func _on_Camera2D_zoom_changed(zoom):
+	camera_zoom=min(1,zoom.x)
